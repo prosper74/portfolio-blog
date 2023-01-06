512c002ccf0f28fa103f87110ef834f4d352f33d
Prosper Atu <atu.prosper@gmail.com>
2023-01-06 08:01:50 +0100

    Changed the import/export style of the home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
-import { Banner } from '../components/home/Banner';
-import { Portfolio } from '../components/home/Portfolio';
-import { Services } from '../components/home/Services';
-import { Skills } from '../components/home/Skills';
+import { Banner, Portfolio, Services, Skills } from '../components/home';
 import SEO from '../components/Seo';
 
 export default function Home() {
