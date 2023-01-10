a3f86c5d68e36358d4e73e01c96f67fe0f8dad70
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2023-01-10 01:13:43 +0100

    FInished the footer section

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,9 +1,38 @@
+import { GithubAltIcon, LinkedinAltIcon } from '../common/svgIcons';
+
 export function Footer() {
   return (
-    <main>
-      <div className="text-3xl text-green-600 p-2">
-        Footer section
-      </div>
-    </main>
+    <footer>
+      <header className="md:fixed md:bottom-0 md:inset-x-0 w-full font-medium bg-primary max-md:mb-8">
+        <div className="flex justify-between items-center gap-5 sm:container mx-auto px-2 md:px-10 lg:px-20 py-3">
+          <div>
+            <p className="font-mono">
+              &copy; {new Date().getFullYear()},
+              <span> Learning in Progress...</span>
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <a
+              href="https://github.com/prosper74"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GithubAltIcon width="25" height="25" fill="rgb(253, 245, 255)" />
+            </a>
+            <a
+              href="https://github.com/prosper74"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LinkedinAltIcon
+                width="25"
+                height="25"
+                fill="rgb(253, 245, 255)"
+              />
+            </a>
+          </div>
+        </div>
+      </header>
+    </footer>
   );
 }
