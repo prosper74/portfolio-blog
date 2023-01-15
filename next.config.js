1bfbbe4847279955c86e36e7e97f1fe2efa2b34f
Prosper Atu <atu.prosper@gmail.com>
2023-01-15 15:00:55 +0100

    Added rewrite to the config file

--- next.config.js
+++ next.config.js
@@ -6,27 +6,11 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  async headers() {
+  async rewrites() {
     return [
       {
-        // matching all API routes
         source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://atuprosper.com',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
+        destination: 'https://portfolio-blog-new.vercel.app/:path*',
       },
     ];
   },
