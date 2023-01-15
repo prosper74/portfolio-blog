d8422bb4e623b33e556ac8f6ad49c04d6ab6d4db
Prosper Atu <atu.prosper@gmail.com>
2023-01-15 14:53:10 +0100

    modified the config file again

--- next.config.js
+++ next.config.js
@@ -13,7 +13,10 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://atuprosper.com',
+          },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,PUT',
