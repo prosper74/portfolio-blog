3f22d8be067084a40e9960b5227a9e8f6a0cfb76
Prosper Atu <atu.prosper@gmail.com>
2023-01-15 15:16:23 +0100

    Still trying to fix cors

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://portfolio-blog-new.vercel.app/:path*',
+        source: '/blog/:path*',
+        destination: 'https://portfolio-blog-new.vercel.app/blog/:path*',
       },
     ];
   },
