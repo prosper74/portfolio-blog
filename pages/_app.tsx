c05efe0277461105a7203da3e983f69d4265271c
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2023-07-21 11:34:11 +0100

    Fixed rehydration error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,34 @@
+import { Fragment } from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { AnimatePresence } from 'framer-motion';
 import { CookiesProvider } from 'react-cookie';
 import Layout from '../components/layout';
+import { useIsClient } from '../components/common/hooks/useIsClient';
 
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { isClient, key } = useIsClient();
+
+  if (!isClient) return null;
+
   return (
-    <CookiesProvider>
-      <ThemeProvider enableSystem={true} attribute="class">
-        <AnimatePresence
-          mode="wait"
-          initial={false}
-          onExitComplete={() => window.scrollTo(0, 0)}
-        >
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </AnimatePresence>
-      </ThemeProvider>
-    </CookiesProvider>
+    <Fragment key={key}>
+      <CookiesProvider>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <AnimatePresence
+            mode="wait"
+            initial={false}
+            onExitComplete={() => window.scrollTo(0, 0)}
+          >
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </AnimatePresence>
+        </ThemeProvider>
+      </CookiesProvider>
+    </Fragment>
   );
 }
